65f0135e247ba63a0dcb0e9def268467bb8fc1bc
changliu <liuchang5454@gmail.com>
2020-04-04 22:13:02 +0800

    till 80 step 20. Add currency conversion service. Call currency exchange service from conversion service in order to get the conversionMultiple.

---
+++ currency-conversion-services/src/main/java/com/springcloud/microservices/currencyconversionservices/CurrencyConversionController.java
@@ -0,0 +1,46 @@
+package com.springcloud.microservices.currencyconversionservices;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class CurrencyConversionController {
+	
+	@GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversionBean convertCurrency(
+			@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity
+			) {
+		
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("from", from);
+		uriVariables.put("to", to);
+		
+		ResponseEntity<CurrencyConversionBean> responseEntity = new RestTemplate().getForEntity(
+				"http://localhost:8000/currency-exchange/from/{from}/to/{to}",
+				CurrencyConversionBean.class,
+				uriVariables
+				);
+		
+		CurrencyConversionBean response = responseEntity.getBody();
+		
+		System.out.println("response : " + response);
+//		BigDecimal total = quantity.multiply(response.getConversionMultiple());
+//		System.out.println("response:"+response.getConversionMultiple());
+//		System.out.println(response.getId()+"::"+quantity.multiply(response.getConversionMultiple()));
+		
+		
+		return new CurrencyConversionBean(response.getId(), from, to, 
+				response.getConversionMultiple(), quantity, 
+				//quantity.multiply(response.getConversionMultiple()), 
+				//above will report a null pointer exception, results in a 500 error
+				new BigDecimal(5555),
+				response.getPort());
+	}
+}
